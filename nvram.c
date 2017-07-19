ba88b5453df484deed507a8dc934db80dd01ddfd
howerj <howe.r.j.89@gmail.com>
2017-07-19 21:13:41 +0100

    Updated code and explanation.
    No further commits are really necessary, the program is complete as it ever
    will be, perhaps the "readme.md" could be updated, or even generated from the C
    code, but that is all.

--- nvram.c
+++ nvram.c
@@ -26,6 +26,10 @@
  * can some how work out the start and end location a section. GCC provides all
  * of these mechanisms.
  *
+ * We will refer to variables that have been augmented with this capability as
+ * NVRAM variables - for "Non-Volatile RAM", as unlike normal variables in C,
+ * their value is kept across program runs.
+ *
  * A variable can be placed into a section with the section attribute, for
  * example if we want to place an integer 'a', into a section called "nvram":
  *
@@ -40,32 +44,54 @@
  * create two variables, "__start_nvram" and "__stop_nvram" which can be used
  * to find out the start and end of NVRAM respectively.
  *
- * We can take this technique a step further, currently the programmer has to
- * worry about the Non-Volatile variables being saved to disk.
- *
+ * We can take this technique a step further and have the variables
+ * automatically saved to disk at program exit, we can do this by using the
+ * "atexit" function, which registers a callback to called when the program
+ * exists normally. This has the added benefit that if an "assert" fails, or a
+ * signal is not caught, then the data will not be saved as it is presumably
+ * corrupt. The programmer now only has to worry about calling an 
+ * initialization function (defined as "nvram_initialize" below).
  * 
  * This technique has various portability problems and version problems, which
  * can be remedied but require a deeper understanding of a specific toolchain,
  * discipline from the programmer, or extra code not shown here.
  *
- * Portability problems:
+ * Limitations:
+ * - The method of course does not work for dynamically allocated variables,
+ *   only for variables of a static storage duration. It must be known at
+ *   compile time the size of the data structures to be stored.
+ *
+ * Consistency Problems:
+ * - The program below may fail only do a partial update or load, these
+ *   consistency problems can be solved in code, but are not needed for a
+ *   simple demonstration.
+ *
+ * Portability Problems:
  * - The alignment between variables stored in a section needs to be
  *   controlled.
- * - Only fixed width types can be used.
+ * - Only fixed width types can be used (portably)
  * - The endianess of variables can differ between machines, however it can be
- *   checked for.
+ *   checked for, correcting for it when it differs is a different matter
+ *   however.
  * - The order in which variables are stored by the linker is not specified.
  * - If Non-Volatile variables are declared in multiple files then the order
  *   object files are passed to the linker could change the format of the file
- *   stored on disk
+ *   stored on disk.
+ * - Most of these problems can be solved by only storing a single struct
+ *   per section, with "__attribute__((packed))", however this makes the
+ *   variables more awkward to use. 
  * 
- * Version incompatibility problems:
+ * Version Incompatibility Problems:
  * - If a variables data storage type is changed, or if a variable is added
  *   or removed, this will invalidate any previously stored files.
  * - A checksum of the default values can be calculated and stored, along with
  *   the size of the section used to store the NVRAM variables, and perhaps a
  *   version number. If either of these does not matched the stored data the
  *   data would have to be regenerated.
+ *
+ * https://gcc.gnu.org/onlinedocs/gcc-4.0.4/gcc/Type-Attributes.html
+ * https://stackoverflow.com/questions/16751378
+ * https://gcc.gnu.org/ml/gcc-help/2013-10/msg00047.html
  *
  */
 
@@ -78,16 +104,26 @@
 #include <assert.h>
 #include <stdbool.h>
 
-const char *nvram_name = "nvram.blk"; /**< file to store NVRAM variables in */
-extern char __start_nvram; /**< placed by GCC denotes start of section 'nvram' */
-extern char __stop_nvram;  /**< placed by GCC denotes end   of section 'nvram' */
-#define NVRAM __attribute__((section("nvram")))
+/* ======= NVRAM Setup ===================================================== */
 
-NVRAM uint16_t endianess = 0x1234;
-NVRAM int32_t nv_a = 0;
-NVRAM int32_t nv_b = 0;
-NVRAM uint8_t nv_buffer[32] = { 0 };
+const  char *nvram_name = "nvram.blk";          /**< file to store NVRAM variables in */
+extern char __start_nvram;                      /**< start of section 'nvram' */
+extern char __stop_nvram;                       /**< end   of section 'nvram' */
+#define NVRAM __attribute__((section("nvram"))) /**< used to put a variable in 'NVRAM' */
 
+/* ======= NVRAM Setup ===================================================== */
+
+/* ======= NVRAM Variables ================================================= */
+
+       NVRAM uint16_t nv_endianess = 0x1234u;   /**< if the endianess of a save block differs this will be 0x3412 */
+static NVRAM int32_t  nv_a = 0;                 /**< example NVRAM variable 'a' */
+static NVRAM int32_t  nv_b = 0;                 /**< example NVRAM variable 'b' */
+static NVRAM uint32_t nv_count = 0;             /**< this variable is incremented each time the program is run */
+
+/* ======= NVRAM Variables ================================================= */
+
+/* ======= Utility Functions =============================================== */
+/**< Transfer a block of memory to (read = false) or from (read = true) disk  */
 static int block(char *buffer, size_t length, const char *name, bool read)
 {
 	FILE *l = NULL;
@@ -96,15 +132,23 @@ static int block(char *buffer, size_t length, const char *name, bool read)
 	assert(name);
 
 	errno = 0;
-       	l = fopen(name, read ? "rb" : "wb");
-	if(!l) {
-		fprintf(stderr, "block %s from '%s' failed: %s\n", read ? "load" : "save", name, strerror(errno));
+	l = fopen(name, read ? "rb" : "wb");
+	if (!l) {
+		fprintf(stderr, "block %s from '%s' failed: %s\n", 
+				read ? "load" : "save", 
+				name, 
+				strerror(errno));
 		return -1;
 	}
 	errno = 0;
 	r = read ? fread(buffer, 1, length, l) : fwrite(buffer, 1, length, l);
-	if(r != length) { 
-		fprintf(stderr, "warning - partial block operation (%zu/%zu bytes %s): %s\n", r, length, read ? "read" : "wrote", strerror(errno));
+	if (r != length) {
+		fprintf(stderr, "warning - partial block operation "
+				" (%zu/%zu bytes %s): %s\n", 
+				r, 
+				length, 
+				read ? "read" : "wrote", 
+				strerror(errno));
 		fclose(l);
 		return -1;
 	}
@@ -112,38 +156,57 @@ static int block(char *buffer, size_t length, const char *name, bool read)
 	return 0;
 }
 
+/**< function to register with atexit, this saves the block to disk */
 static void nvram_save(void)
 {
-	if(block(&__start_nvram, &__stop_nvram - &__start_nvram, nvram_name, false)) {
-		fprintf(stderr, "nvram block save failed: '%s' will not be updated\n", nvram_name);
+	fprintf(stderr, "saving nvram to '%s'\n", nvram_name);
+	if (block(&__start_nvram, &__stop_nvram - &__start_nvram, nvram_name, false)) {
+		fprintf(stderr, "nvram block save failed: '%s'\n", nvram_name);
 	}
 }
 
-static int nvram_initialize(void) 
+/**< register save call back atexit and load in NVRAM variables */
+static int nvram_initialize(void)
 {
-	if(atexit(nvram_save)) {
+	int r = 0;
+	if (atexit(nvram_save)) {
 		fputs("atexit: failed to register nvram_save\n", stderr);
-		return -1;
+		r = -1;
 	}
-	if(block(&__start_nvram, &__stop_nvram - &__start_nvram, nvram_name, true)) {
+	if (block(&__start_nvram, &__stop_nvram - &__start_nvram, nvram_name, true)) {
 		fputs("nvram block load failed: default values will be used\n", stderr);
-		return -1;
+		r = -1;
 	}
-	return 0;
+	return r;
 }
 
+/* ======= Utility Functions =============================================== */
+
+/* ======= Test Program ==================================================== */
+/* A simple test program for the techniques described above, it prints the
+ * default values for NVRAM variables, initializes the NVRAM and registers the
+ * save callback, and allows the user to update values which will be saved to
+ * disk on exit. */
 int main(void)
 {
+	/* default values can be accessed before nvram_initialize is called */
+	printf("default a:   %d\n", (int)nv_a);
+	printf("default b:   %d\n", (int)nv_b);
+
+	/* loads in variables off disk and registers nvram_initialize */
 	nvram_initialize();
 
-	printf("a: %d\n", (int)nv_a);
-	printf("b: %d\n", (int)nv_b);
+	printf("count:       %u\n", (unsigned)(nv_count++));
+	printf("loaded a:    %d\n", (int)nv_a);
+	printf("loaded b:    %d\n", (int)nv_b);
 
-	fputs("Enter new value for 'a': ", stdout);
-	scanf("%"SCNd32, &nv_a);
-	fputs("Enter new value for 'b': ", stdout);
-	scanf("%"SCNd32, &nv_b);
+	fputs("a new value: ", stdout);
+	scanf("%" SCNd32, &nv_a);
+	fputs("b new value: ", stdout);
+	scanf("%" SCNd32, &nv_b);
+
+	/* We do not have to worry about calling nvram_save, atexit will */
 
 	return 0;
 }
-
+/* ======= Test Program ==================================================== */
--- readme.md
+++ readme.md
@@ -3,9 +3,40 @@
 This is project presents a small tutorial for a technique to automatically
 store and load designated variables with minimal hassle for the programmer. The
 variables can be declared normally, without massive amounts of serialization
-or deserialization code. The method is not without limitations however.
+or deserialization code. 
+
+The method is not without limitations or problems, but it is quick and simple
+to implement.
 
 The tutorial is written in [nvram.c][], this file should be viewed to see how
-it works.
+it works, **spoiler** - get your [linker][] to put your variables in a custom
+section and write this to disk using [atexit][].
+
+This code works with either [GCC][] or [Clang][].
+
+## Perquisites 
+
+* Either [GCC][] or [Clang][] must be installed and on the PATH.
+* As must [GNU Make][]
+
+## Building
+
+To build and run the program:
+
+	make nvram
+	make run
+
+When run, the nvram program will attempt to load a file off disk called
+"nvram.blk", if it cannot do that then the default values as specified in the C
+source code are used.
+
+The variables get written back to disk on program exit, the demonstration
+program does nothing fancy, it is simply there to illustrate the technique.
+
 
 [nvram.c]: nvram.c
+[linker]: https://en.wikipedia.org/wiki/Linker_(computing)
+[atexit]: http://man7.org/linux/man-pages/man3/atexit.3.html
+[GCC]: https://gcc.gnu.org/
+[Clang]: https://clang.llvm.org/
+[GNU Make]: https://www.gnu.org/software/make/
